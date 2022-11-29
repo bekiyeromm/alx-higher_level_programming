bdf193307cffe04bbdd1dc5fee4b66f84b9acc38
bekiyeromm <bk3tena@gmail.com>
2022-11-29 05:44:01 -0800

    printing numbers from 0 to 98

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0,100):
+for i in range (0, 100):
     if(i < 99):
         if i < 10:
-                i = '0'+str(i)
+                i = '0' + str(i)
         print("{}".format(i), end =", ")
     else:
-        print("{}".format(i, end = "\n"))
+        print("{}".format(i), end = "\n")
