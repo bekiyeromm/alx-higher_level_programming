9c07613e41106cc74784780a00d13a246a0c95ce
bekiyeromm <bk3tena@gmail.com>
2022-11-29 05:39:43 -0800

    printing number 1 upto 99

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print("{}{}".format(i % 10, j % 10), end = "")
+        print("{}{}".format((i % 10), (j % 10)), end = "")
         if i == 8 and j == 9:
             continue
         print(", ", end = "")
