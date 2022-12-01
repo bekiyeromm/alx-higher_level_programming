ef3d014b4202f0ef6f2128de91d0c295715b5499
bekiyeromm <bk3tena@gmail.com>
2022-12-01 09:36:23 -0800

    Write a program that prints the number of and the list of its arguments. the output should be Number of argument(s) followed by argument (if number is one) or arguments (otherwise), followed by : (or . if no arguments were passed) followed by.a new line, followed by (if at least one argument), one line per argument. the position of the argument (starting at 1) followed by :, followed by the argument value and a new line

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
+    arg_count = len(argv)
+    i = 1
+    if arg_count == 0:
+        print("{:d} arguments.".format(arg_count))
+    elif arg_count == 1:
+         print("{:d} argument:".format(arg_count))
+         print("{:d}: {:s}".format(i, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(arg_count))
+        while i <= arg_count:
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i = i + 1
