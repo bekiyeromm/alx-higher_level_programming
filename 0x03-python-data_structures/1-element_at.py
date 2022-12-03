7a31a41e8ee8c88fca4b9eae7d385153034961fe
bekiyeromm <bk3tena@gmail.com>
2022-12-03 08:58:05 -0800

    Write a function that retrieves an element from a list like in C. Prototype: def element_at(my_list, idx): If idx is negative, the function should return None. If idx is out of range (> of number of element in my_list), the function should return None. ou are not allowed to import any module. You are not allowed to use try/except

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for i in range(len(my_list)):
-        if(idx < -6 or idx >len(my_list)):
+        if(idx < -6 or idx >len(my_list) - 1):
             exit(0)
         else:
             return (my_list[idx])
