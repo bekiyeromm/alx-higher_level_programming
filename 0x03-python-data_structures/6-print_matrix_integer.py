ee4bbb15afe4b5cfe78c51e54525ab7bd81f4eab
bekiyeromm <bk3tena@gmail.com>
2022-12-04 22:08:58 -0800

    Write a function that prints a matrix of integers.Prototype: def print_matrix_integer(matrix=[[]]): You have to use str.format() to print integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if  matrix:
-        for i in matrix:
-            for j in i:
-                if j != i[- 1]:
-                    sep = " "
-                else:
-                    sep = ""
-                print("{:d}".format(j), end=sep)
-            print()
+    for i in matrix:
+        for j in i:
+            if j != i[- 1]:
+                sep = " "
+            else:
+                sep = ""
+            print("{:d}".format(j), end=sep)
+        print()
 
