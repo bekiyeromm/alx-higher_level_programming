973af7dc888209398cf5081c3d94d25fd90968af
bekiyeromm <bk3tena@gmail.com>
2022-12-05 08:51:53 -0800

    Write a function that adds 2 tuples Prototype: def add_tuple(tuple_a=(), tuple_b=()): Returns a tuple with 2 integers The first element should be the addition of the first element of each argument and so on. If a tuple is smaller than 2, use the value 0 for each missing integer If a tuple is bigger than 2, use only the first 2 integers

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,5 +11,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = tuple_b + temp
     elif (len(tuple_b) > 2):
         tuple_b = tuple_b[:2] + temp
-    sum_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_a[1]
+    sum_tuple = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return (sum_tuple)
