9230ab59ff6a2dd979fa7b413cae176fd2534964
bekiyeromm <bk3tena@gmail.com>
2022-12-06 02:47:57 -0800

    Write a function in C that checks if a singly linked list is a palindrome. Prototype: int is_palindrome(listint_t **head); Return: 0 if it is not a palindrome, 1 if it is a palindrome. An empty list is considered a palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ * is_palindrome - checks if palindrome
+ * @head: head of node
+ * Return: 0 if not, 1 if is
+ */
+
+int is_palindrome(listint_t **head)
+{
+	unsigned int len = 1;
+	listint_t *temp;
+
+	if (head == NULL|| *head == NULL)
+		return (0);
+	temp = *head;
+	while (temp) /* get len of list */
+	{
+		temp = temp->next;
+		len++;
+	}
+	return (1);
+}
