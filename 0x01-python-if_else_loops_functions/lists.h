6585c8df386047c0af999f37d9bbda35cbe74642
bekiyeromm <bk3tena@gmail.com>
2022-12-09 03:08:08 -0800

    lists.h filr

---
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -0,0 +1,16 @@
+#ifndef LIST_H
+#define LIST_H
+#include <stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<stddef.h>
+def islower(c);
+def uppercase(str);
+def print_last_digit(number);
+def add(a, b);
+def pow(a, b);
+def fizzbuzz();
+listint_t *insert_node(listint_t **head, int number);
+
+#endif
