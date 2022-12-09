5680e414aa9f09c8e37c5e43b82445d2f915cde6
bekiyeromm <bk3tena@gmail.com>
2022-12-09 03:22:24 -0800

    lists.h file

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -5,6 +5,21 @@
 #include<math.h>
 #include<stdlib.h>
 #include<stddef.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer to be added
+ * @next: pointer to the next node
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *insert_node(listint_t **head, int number);
+void free_listint(listint_t *head);
+size_t print_listint(const listint_t *h);
+
 
 #endif
