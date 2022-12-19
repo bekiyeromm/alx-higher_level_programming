84677e9f957de70f1a78d755b173dfbbf3501fe0
bekiyeromm <bk3tena@gmail.com>
2022-12-19 03:05:24 -0800

    Write a function that prints x elements of a list.
    Prototype: def safe_print_list(my_list=[], x=0):
    my_list can contain any type (integer, string, etc.)
    All elements must be printed on the same line followed by a new line.
    x represents the number of elements to print
    x can be bigger than the length of my_list
    Returns the real number of elements printed
    You have to use try: / except:
    You are not allowed to import any module
    You are not allowed to use len()

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             count += 1
-        except continue
+        except Exception:
+            continue
     print()
     return count
