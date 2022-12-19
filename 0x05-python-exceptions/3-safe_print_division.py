87e1a558d24dc98a951818898ba829a8a8770189
bekiyeromm <bk3tena@gmail.com>
2022-12-19 03:13:09 -0800

    Write a function that divides 2 integers and prints the result.
    Prototype: def safe_print_division(a, b):
    You can assume that a and b are integers
    The result of the division should print on the finally: section preceded by Inside result:
    Returns the value of the division, otherwise: None
    You have to use try: / except: / finally:
    You have to use {}.format() to print the result
    You are not allowed to import any module

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     result = 0
     try:
         result = a / b
-    except Exception:
+    except:
         return == None
     finally:
         print("Inside result: {}".format(result))
