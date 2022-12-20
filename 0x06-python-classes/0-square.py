f1bc83a42d26aa58fba3468872975475f1b874a6
bekiyeromm <bk3tena@gmail.com>
2022-12-20 10:24:35 -0800

    Write an empty class Square that defines a square:
    You are not allowed to import any module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,10 @@
-#!/u:sr/bin/python3
+#!/usr/bin/python3
+"""Square module.
+This module contains a class that defines a square.
+"""
 
-"""class Square
 
-an empty class square."""
+class Square():
+    """Defines a square."""
 
-
-class Square:
     pass
