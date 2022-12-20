3db5abe8ccea50ed564b2cb36d3198d62341b4e0
bekiyeromm <bk3tena@gmail.com>
2022-12-19 18:10:15 -0800

    Write a function that executes a function safely.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         elif sys.argv[2] == "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] == "*":
+        elif sys.argv[2] == ("*"):
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
