304740740a666eeb3a48722ecbf5ddc6a5489a74
bekiyeromm <bk3tena@gmail.com>
2022-12-19 18:18:33 -0800

    Write a function that executes a function safely.
    Prototype: def safe_function(fct, *args):
    You can assume fct will be always a pointer to a function
    Returns the result of the function,
    Otherwise, returns None if something happens during the function and prints in stderr the error precede by Exception:
    You have to use try: / except:

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file = sys.stderr)
+    except Exception:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
