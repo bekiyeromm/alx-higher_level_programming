3d6ae44ee8dd264befd01c51c06fc12dcd0c5aae
bekiyeromm <bk3tena@gmail.com>
2023-01-02 01:26:12 -0800

    Since the beginning you have been creating “Interactive tests”. For this exercise, you will add Unittests.
    In this task, you will write unittests for the function def max_integer(list=[]):.
    
    Your test file should be inside a folder tests
    You have to use the unittest module
    Your test file should be python files (extension: .py)
    Your test file should be executed by using this command: python3 -m unittest tests.6-max_integer_test
    All tests you make must be passable by the function below
    We strongly encourage you to work together on test cases, so that you don’t miss any edge case

---
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(list=[]):
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
