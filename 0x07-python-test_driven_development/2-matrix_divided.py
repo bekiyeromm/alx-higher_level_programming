0ca645aee3a4922841151cb99ae0b4bdddb67491
bekiyeromm <bk3tena@gmail.com>
2023-01-02 01:22:15 -0800

    Write a function that divides all elements of a matrix.
    Prototype: def matrix_divided(matrix, div):
    matrix must be a list of lists of integers or floats, otherwise raise a TypeError exception with the message matrix must be a matrix (list of lists) of integers/floats
    Each row of the matrix must be of the same size, otherwise raise a TypeError exception with the message Each row of the matrix must have the same size
    div must be a number (integer or float), otherwise raise a TypeError exception with the message div must be a number
    div can’t be equal to 0, otherwise raise a ZeroDivisionError exception with the message division by zero
    All elements of the matrix should be divided by div, rounded to 2 decimal places
    Returns a new matrix
    You are not allowed to import any module

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""module matrix_dividde
-
-divides two matrix element
-"""
-
-
 def matrix_divided(matrix, div):
-    """divides matrix element
-    Args:
-        matrix (list): matrix element to be divided
-        div (int, float) divider
-    """
+    message = "matrix must be a matrix (list of lists) of integers/floats"
+    if not matrix:
+        raise TypeError(message)
+    if not isinstance(matrix, list):
+        raise TypeError(message)
+    for lists in matrix:
+        if not isinstance(lists, list):
+            raise TypeError(message)
+        for item in lists:
+            if not isinstance(item, (float, int)):
+                raise TypeError(message)
+    for lists in matrix:
+         if len(lists) == 0:
+             raise TypeError(message)
+    if not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not all(len(lists) == len(matrix[0]) for lists in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
     return([[round(j / div, 2) for j in i] for i in matrix])
