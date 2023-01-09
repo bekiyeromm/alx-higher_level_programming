02fc3f44b09fb6d3bd193b491558de67a474d607
bekiyeromm <bk3tena@gmail.com>
2023-01-09 09:34:23 -0800

    Write a class MyList that inherits from list:
    Public instance method: def print_sorted(self): that prints the list, but sorted (ascending sort)
    You can assume that all the elements of the list will be of type int
    You are not allowed to import any module

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a module named MyList that inherited from module list
+and prints alist in sorted (ascending sort)
+"""
+
+
+class MyList(list):
+    """class Mylist inherited from class list
+    with a method print_sorted that prints a list in sorted
+    order"""
+
+    def print_sorted(self):
+        """prints a lists sorted in ascending order"""
+        print(sorted(list(self)))
