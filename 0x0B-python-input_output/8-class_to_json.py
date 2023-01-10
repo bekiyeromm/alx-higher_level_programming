78effcaf96ad4731d464f9df6b3d875ff5c1b32c
bekiyeromm <bk3tena@gmail.com>
2023-01-10 10:34:32 -0800

    Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object:
    Prototype: def class_to_json(obj):
    obj is an instance of a Class
    All attributes of the obj Class are serializable: list, dictionary, string, integer and boolean
    You are not allowed to import any module

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module named class_to_json module"""
+
+
+def class_to_json(obj):
+    """returns dict descriotion of obj"""
+
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__.copy()
+    else:
+        return {}
