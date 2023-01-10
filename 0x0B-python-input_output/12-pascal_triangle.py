bc817da7d974eb4027a4a00733b55646c9e36ae0
bekiyeromm <bk3tena@gmail.com>
2023-01-10 10:49:15 -0800

    Technical interview preparation:
    You are not allowed to google anything
    Whiteboard first
    Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:
    
    Returns an empty list if n <= 0
    You can assume n will be always an integer
    You are not allowed to import any module

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module for pascal_triangle"""
+
+
+def pascal_triangle(n):
+    """a method to create a pascal_triangle"""
+    triangle = []
+    line = [1]
+    y = [0]
+    for x in range(max(n, 0)):
+        triangle.append(line)
+        line = [l + r for l, r in zip(line + y, y + line)]
+    return triangle
