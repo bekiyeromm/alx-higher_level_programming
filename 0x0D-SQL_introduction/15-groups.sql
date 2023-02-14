9e4ca131e8404ecb31b072e2269b1027db31ea5d
bekiyeromm <bk3tena@gmail.com>
2023-02-14 10:51:57 -0800

    Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
    The result should display:
    the score
    the number of records for this score with the label number
    The list should be sorted by the number of records (descending)
    The database name will be passed as an argument to the mysql command

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+-- lists the number of records with the same score in 
+-- the table second_table
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC
