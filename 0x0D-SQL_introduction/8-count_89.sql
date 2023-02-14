bb9aac77036a56a2ec6cff9899ae19ac2080a0c5
bekiyeromm <bk3tena@gmail.com>
2023-02-14 10:08:21 -0800

    Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
    The database name will be passed as an argument of the mysql command

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 -- script that displays the number of records with id = 89
--- in the table first_table of the database hbtn_0c_0
 SELECT COUNT(id) from first_table WHERE id = 89;
