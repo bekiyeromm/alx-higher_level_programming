d9342ad5ebc875cad2529fac4dda364f3644fd22
bekiyeromm <bk3tena@gmail.com>
2023-02-15 07:03:07 -0800

    Write a script that creates the MySQL server user user_0d_1.
    user_0d_1 should have all privileges on your MySQL server
    The user_0d_1 password should be set to user_0d_1_pwd
    If the user user_0d_1 already exists, your script should not fail

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,6 @@
+#!/usr/bin/env bash
 -- Creates the user user_0d_1 with all privileges.
 -- on mysql server
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-	IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-	ON *.* TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
