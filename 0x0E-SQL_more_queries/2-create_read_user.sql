da58f6162d42a9fb7be4cf91da43c5994c565448
bekiyeromm <bk3tena@gmail.com>
2023-02-15 07:12:05 -0800

    Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
    user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
    The user_0d_2 password should be set to user_0d_2_pwd
    If the database hbtn_0d_2 already exists, your script should not fail
    If the user user_0d_2 already exists, your script should not fail

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
--- script that creates the database hbtn_0d_2 and the user user_0d_2
-CREATE USER IF NOT EXISTS user_0d_2@localhost;
-	IDENTIFIED BY user_0d_2_pwd;
+-- script that creates the database hbtn_0d_2
+-- and the user user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.*
-	TO user_0d_2@localhost
-	IDENTIFIED BY user_0d_2_pwd;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
