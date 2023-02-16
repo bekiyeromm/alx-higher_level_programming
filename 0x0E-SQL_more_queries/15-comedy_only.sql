478befdaa3043b0b2f79803a97e1444904dfe311
bekiyeromm <bk3tena@gmail.com>
2023-02-15 21:13:57 -0800

    Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 14-my_genres.sql)
    Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
    
    The tv_genres table contains only one record where name = Comedy (but the id can be different)
    Each record should display: tv_shows.title
    Results must be sorted in ascending order by the show title
    You can use only one SELECT statement
    The database name will be passed as an argument of the mysql command

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server
 -- lists all Comedy shows in the database hbtn_0d_tvshows
 -- tv_genres table contains only one record where name = Comedy
 -- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
+-- You can use only one SELECT statement
 SELECT title FROM tv_shows
 JOIN tv_show_genres ON id=tv_show_genres.show_id
 JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
