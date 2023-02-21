746c5491eacbfc4aac52358cb20eee385ad526ff
bekiyeromm <bk3tena@gmail.com>
2023-02-20 22:34:38 -0800

    Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
    Write a script that displays the max temperature of each state (ordered by State name).

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 -- displays the 3 max temperature of each state (ordered by State name).
-SELECT state, MAX(value) AS temp_max FROM temperatures
+SELECT state, MAX(value) AS max_temp FROM temperatures
 GROUP BY state
 ORDER BY state
 LIMIT 3;
