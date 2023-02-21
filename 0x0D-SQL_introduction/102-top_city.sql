77968b0108a5526a240cd8512cfa15ad95937f45
bekiyeromm <bk3tena@gmail.com>
2023-02-20 22:26:40 -0800

    Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
    Write a script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 -- displays the top 3 of cities temperature during July and August 
-SELECT `city`, AVG(`value`) AS `temp`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
-ORDER BY `temp` DESC
+ORDER BY `avg_temp` DESC
 LIMIT 3;
