5a62fc086c6858b1ae68db4d0b6220a74857bf51
bekiyeromm <bk3tena@gmail.com>
2023-03-13 12:18:12 -0700

    Write a script that searches the second biggest integer in the list of arguments.
    You can assume all arguments can be converted to integer
    If no argument passed, print 0
    If the number of arguments is 1, print 0
    You must use console.log(...) to print all output
    You are not allowed to use var
    guillaume@ubuntu:~/0x12$ ./11-second_biggest.js
    0
    guillaume@ubuntu:~/0x12$ ./11-second_biggest.js 1
    0
    guillaume@ubuntu:~/0x12$ ./11-second_biggest.js 4 2 5 3 0 -3
    4
    guillaume@ubuntu:~/0x12$

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,7 @@ const args = process.argv.slice(2);
 // Check if there are no arguments
 if (args.length === 0) {
   console.log(0);
-}
-// Check if there is only one argument
-else if (args.length === 1) {
+} else if (args.length === 1) {
   console.log(0);
 } else {
   // Convert arguments to integers
