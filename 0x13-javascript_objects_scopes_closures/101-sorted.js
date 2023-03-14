57d42c7b4e3ad9c8a41d2f5c08c569df641996d5
bekiyeromm <bk3tena@gmail.com>
2023-03-14 11:39:59 -0700

    Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
    Your script must import dict from the file 101-data.js
    In the new dictionary:
    A key is a number of occurrences
    A value is the list of user ids
    Print the new dictionary at the end
    guillaume@ubuntu:~/0x13$ cat 101-data.js
    exports.dict = {
      89: 1,
      90: 2,
      91: 1,
      92: 3,
      93: 1,
      94: 2
    };
    guillaume@ubuntu:~/0x13$ ./101-sorted.js
    { '1': [ '89', '91', '93' ], '2': [ '90', '94' ], '3': [ '92' ] }
    guillaume@ubuntu:~/0x13$

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+function getUserIdsByOccurrence(dict) {
+  const result = {};
+  // Iterate over the user ids in the input dictionary
+  for (const userId in dict) {
+    const occurrence = dict[userId];
+    // Add the user id to the list of ids for this occurrence count
+    if (!result[occurrence]) {
+      result[occurrence] = [];
+    }
+    result[occurrence].push(userId);
+  }
+  return result;
+}
+const userIdsByOccurrence = getUserIdsByOccurrence(dict);
+console.log(userIdsByOccurrence);
