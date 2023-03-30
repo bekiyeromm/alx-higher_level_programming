f00e48a1c8a1feeb112f1f5f9973052cc4b92791
berekettena <tena21bereket@gmail.com>
2023-03-30 19:40:56 +0300

    Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
    You have to use curl
    Please test your script in the sandbox provided, using the web server running on port 5000

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept
-curl -X -s -I OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
+curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
