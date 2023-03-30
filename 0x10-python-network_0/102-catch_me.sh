0ee407d56f313fd58c41db62326b37222dbcefc2
berekettena <tena21bereket@gmail.com>
2023-03-30 21:53:55 +0300

    Write a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me in the body of the response.
    You have to use curl
    You are not allow to use echo, cat, etc. to display the final result
    Please test your script in the sandbox provided, using the web server running on port 5000
    usage:
    guillaume@ubuntu:~/0x10$ ./102-catch_me.sh ; echo
    You got me!
    guillaume@ubuntu:~/0x10$

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-#!/bin/bash
 curl -X PUT 0.0.0.0:5000/catch_me -sw "You got me!"
