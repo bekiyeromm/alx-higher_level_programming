9fbea41bee1c9a3ba60ab0cb26b33128175d7e05
berekettena <tena21bereket@gmail.com>
2023-03-31 17:41:51 +0300

    2. POST an email #0 mandatory Write a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
    The email must be sent in the email variable
    You must use the packages urllib and sys
    You are not allowed to import packages other than urllib and sys
    You don’t need to check arguments passed to the script (number or type)
    You must use the with statement
    Please test your script in the sandbox provided, using the web server running on port 5000
    usage:
    guillaume@ubuntu:~/0x11$ ./2-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
    Your email is: hr@holbertonschool.com
    guillaume@ubuntu:~/0x11$

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email, sends a POST request
-to the passed URL with the email as a parameter, and displays the body
-of the response (decoded in utf-8
-"""
-import urllib.parse
+"""script for testing POST requests so servers"""
+
+
 import urllib.request
+import urllib.parse
 import sys
 
-
-url = sys.argv[1]
-email = sys.argv[2].encode("utf-8")
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('ascii')
-
 if __name__ == "__main__":
-    with urllib.request.urlopen(url, data) as response:
-        resp = response.read()
-        cont = resp.decode("utf-8")
-        print(cont)
+    url = sys.argv[1]
+    payload = {'email': sys.argv[2]}
+    payload = (urllib.parse.urlencode(payload)).encode('ascii')
+    req = urllib.request.Request(url, payload)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
