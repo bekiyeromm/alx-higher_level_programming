ec7b50d6cd059d11de7bd9db01e7c763e739c9a2
berekettena <tena21bereket@gmail.com>
2023-03-31 18:20:56 +0300

    3. Error code #0 mandatory Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
    You have to manage urllib.error.HTTPError exceptions and print: Error code: followed by the HTTP status code
    You must use the packages urllib and sys
    You are not allowed to import other packages than urllib and sys
    You don’t need to check arguments passed to the script (number or type)
    You must use the with statement
    Please test your script in the sandbox provided, using the web server running on port 5000
    usage:
    0x11-python-network_1$ ./3-error_code.py http://34.229.137.228:80/status_401
    Error code: 404

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,14 +4,12 @@ the body of the response (decoded in utf-8)"""
 
 
 import urllib.request
-import urllib.error
 import sys
+import urllib.error
 
-url = sys.argv[1]
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print(body)
+        with urllib.request.urlopen(sys.argv[1]) as resp:
+            print(resp.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
