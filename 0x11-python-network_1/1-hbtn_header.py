b92b12a889e6224afa15507e6e7ab9ecf4524042
berekettena <tena21bereket@gmail.com>
2023-03-31 18:00:55 +0300

    1. Response header value #0 mandatory Write a Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response.
    You must use the packages urllib and sys
    You are not allow to import packages other than urllib and sys
    The value of this variable is different for each request
    You don’t need to check arguments passed to the script (number or type)
    You must use a with statement
    usage:
    guillaume@ubuntu:~/0x11$ ./1-hbtn_header.py https://alx-intranet.hbtn.io
    ade2627e-41dd-4c34-b9d9-a0fa0f47b237
    guillaume@ubuntu:~/0x11$

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """Python script that takes in a URL, sends a request to the URL and
 displays thevalue of the X-Request-Id variable
-found in the header of the response
 """
 
 
 import urllib.request
 import sys
 
-reqst = urllib.request.Request(sys.argv[1])
 if __name__ == "__main__":
-    with urllib.request.urlopen(reqst) as response:
-        request_id = dict(response.headers).get('X-Request-Id')
-        print(request_id)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        for header in response.info()._headers:
+            if header[0] == 'X-Request-Id':
+                print(header[1])
