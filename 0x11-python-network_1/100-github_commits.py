7870ee18dbb85f0f3c5c8c5e9174c433faf08e66
berekettena <tena21bereket@gmail.com>
2023-04-28 22:57:10 +0300

    The Holberton School staff evaluates candidates applying for a back-end position with multiple technical challenges, like this one: Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails” You must use the GitHub API, here is the documentation https://developer.github.com/v3/repos/commits/ Print all commits by:  (one by line) Write a Python script that takes 2 arguments in order to solve this challenge.
    The first argument will be the repository name
    The second argument will be the owner name
    You must use the packages requests and sys
    You are not allowed to import packages other than requests and sys
    You don’t need to check arguments passed to the script (number or type)
    Only 17% of applicants for a backend position at Holberton finished this task in less than 15 minutes

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""list 10 commits (from the most recent to oldest) of the repository
+“rails” by the user “rails
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    res = requests.get(f"https://api.github.com/repos/{owner}/{repo}/commits")
+    # Parse the response JSON data
+    commits = res.json()
+    for commit in commits[0:10:1]:
+        commit_id = commit['sha']
+        author = commit['commit']['author']['name']
+        print(f"{commit_id}: {author}")
