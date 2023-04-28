7195d1f89543ea61ac843a0d9f36583138135961
berekettena <tena21bereket@gmail.com>
2023-04-28 14:24:57 +0300

    Write a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)
    The email must be sent in the email variable
    You must use the packages urllib and sys
    You are not allowed to import packages other than urllib and sys
    You don’t need to check arguments passed to the script (number or type)
    You must use the with statement

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the
+"""Python script that takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and displays the body of the
 response (decoded in utf-8)
 """
 
