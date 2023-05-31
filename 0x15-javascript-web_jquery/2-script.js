736a75f680dfdf70f89747d45b3ccb45e83ee6e9
berekettena <tena21bereket@gmail.com>
2023-05-31 21:15:00 +0300

    Write a JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header:
    You can’t use document.querySelector to select the HTML tag
    You must use the JQuery API

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+    // Add click event listener to the div with id "red_header"
+    $('div#red_header').click(function() {
+      // Update the text color of <header> to red
+      $('header').css('color', '#FF0000');
+    });
